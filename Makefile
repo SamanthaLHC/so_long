86d85d7ebb53ca8fa9a84530ddd851e6e77fb355
Samantha LE HUEC <sle-huec@e2r11p3.clusters.42paris.fr>
2022-03-17 17:20:54 +0100

    ....

--- Makefile
+++ Makefile
@@ -6,23 +6,36 @@
 #    By: sle-huec <sle-huec@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/03/17 14:13:22 by sle-huec          #+#    #+#              #
-#    Updated: 2022/03/17 14:49:34 by sle-huec         ###   ########.fr        #
+#    Updated: 2022/03/17 16:11:59 by sle-huec         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME = so_long
-SRCS = main.c utils.c maps.c
+
+SRCS = so_long.c \
+		utils.c \
+		maps.c \
+		get_next_line.c \
+		get_next_line_utils.c
+		
 OBJ = $(SRCS:%.c=%.o)
+
 CC = cc 
+
 MLX = mlx/libmlx_Linux.a 
+
 $(NAME): $(OBJ) $(MLX)
 	$(CC) $(OBJ) -g3 -Lmlx -lmlx_Linux -L/usr/lib -lXext -lX11 -lm -lz -o $(NAME)
+
 %.o: %.c
-	$(CC) -g3 -Wall -Wextra -Werror -I/usr/include -Imlx -O3 -c $< -o $@
+	$(CC) -g3 -DBUFFER_SIZE=1 -Wall -Wextra -Werror -I/usr/include -Imlx -O3 -c $< -o $@
+
 $(MLX):
 	make -C ./mlx 
+
 clean: 
 	rm -rf $(OBJ)
+
 
 fclean: clean
 	rm -rf $(NAME)
--- so_long.c
+++ so_long.c
@@ -1,12 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   so_long.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sle-huec <sle-huec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/17 14:12:48 by sle-huec          #+#    #+#             */
-/*   Updated: 2022/03/17 14:22:09 by sle-huec         ###   ########.fr       */
+/*   Created: 2022/03/17 16:09:30 by sle-huec          #+#    #+#             */
+/*   Updated: 2022/03/17 16:09:37 by sle-huec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+
+int main (int ac, char **av)
+{
+	if (ac == 2)
+	{
+		int fd;
+		char *line;
+		fd = open(*av, O_RDWR);
+		while (1)
+		{
+			line = get_next_line(fd);
+			//up_maps(&line);
+			free(line);
+		}
+	}
+}
